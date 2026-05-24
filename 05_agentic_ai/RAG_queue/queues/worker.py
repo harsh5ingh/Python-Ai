e0236885e01b9b58b83f404edac13e404e8aeb78
Harsh <harrsh0502@gmail.com>
2026-05-24 23:23:06 +0530

    learned about RAG queue.

---
+++ 05_agentic_ai/RAG_queue/queues/worker.py
@@ -0,0 +1,69 @@
+from dotenv import load_dotenv
+import os
+from langchain_nvidia import NVIDIAEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1",
+)
+
+embedding_model = NVIDIAEmbeddings(
+    model="nvidia/nv-embed-v1",
+)
+
+vector_db = QdrantVectorStore.from_existing_collection(
+    url=os.getenv("QDRANT_URL"),
+    api_key=os.getenv("QDRANT_API_KEY"),
+    collection_name="learning_rag",
+    embedding=embedding_model,
+)
+
+def process_query(query: str):
+    print("Searching Chunks", query)
+
+    search_result = vector_db.similarity_search(query=query)
+
+    context = "\n\n\n".join([
+        f"Page Content: {result.page_content}\n"
+        f"Page Number: {result.metadata['page_label']}\n"
+        f"File Location: {result.metadata['source']}"
+        for result in search_result
+    ])
+
+    SYSTEM_PROMPT = """
+    You are a helpful AI Assistant who answers user query
+    based on the available context retrieved from a PDF file
+    along with page contents and page number.
+
+    You should only answer the user based on the following
+    context and navigate the user to open the right page
+    number to know more.
+    """
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+            },
+            {
+                "role": "user",
+                "content": f"""
+                Context:
+                {context}
+
+                Question:
+                {query}
+                """
+            },
+        ]
+    )
+
+    print(f"🤖: {response.choices[0].message.content}")
+
+    return response.choices[0].message.content
